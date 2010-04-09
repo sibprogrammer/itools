2be40444da0c1d119e1c90313414b1f91bc8318c
sibprogrammer <sibprogrammer@mail.ru>
2010-04-09 06:43:45 +0400

    Import project's code.

---
+++ app/controllers/service_controller.rb
@@ -0,0 +1,79 @@
+require 'digest/md5'
+
+class ServiceController < ApplicationController
+  before_filter :auth
+  before_filter :post_required, :get_code, :except => :index
+  
+  def index
+    @pages = AppConfig.pages
+    @contacts_link = AppConfig.contacts.link
+  end
+  
+  def php
+    execute("<?php\n" + @code + "\n?>", 'php', '.php')
+  end
+  
+  def ruby
+    execute(@code, 'ruby', '.rb')
+  end
+  
+  def bash
+    execute(@code, 'bash', '.sh')
+  end
+  
+  def python
+    execute(@code, 'python', '.py')
+  end
+  
+  def perl
+    execute(@code, 'perl', '.pl')
+  end
+  
+  private
+  
+  def auth
+    return true if !AppConfig.auth.enabled
+    authenticate_or_request_with_http_basic do |login, password| 
+      login == AppConfig.auth.login && Digest::MD5.hexdigest(password) == AppConfig.auth.password
+    end
+  end
+  
+  def post_required
+    redirect_to :action => 'index' and return false if request.get?
+  end
+  
+  def get_code
+    @code = params['code']
+    
+    return false if !valid_form { |errors|
+      validate_not_empty(errors, 'code', @code)
+    }
+  end
+  
+  def valid_form
+    errors = []
+    yield errors
+    render :json => { :errors => errors } and return false if !errors.empty?
+    return true
+  end
+  
+  def validate_not_empty(errors, field_name, field)
+    errors.push({ :field => field_name, :text => 'Fill the field' }) if field.blank?
+  end
+  
+  def get_random_hash
+    chars =  [('a'..'f'),(0..9)].map{ |i| i.to_a }.flatten
+    (0..31).map{ chars[rand(chars.length)] }.join
+  end
+  
+  def execute(code, command, extension = '')
+    filename = '/tmp/itools_' + get_random_hash + extension
+    File.open(filename, 'w') { |f| f.write(code) }
+    logger.info("Command (IP #{request.remote_ip}): #{command} #{filename}")
+    @output = `sudo -u nobody env TERM=xterm #{command} #{filename} 2>&1`
+    File.delete(filename)
+    
+    render :json => { :status => $?.success?, :output => @output }
+  end
+  
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,183 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Object.extend(Event, {
+  _domReady : function() {
+    if (arguments.callee.done) return;
+    arguments.callee.done = true;
+
+    if (Event._timer)  clearInterval(Event._timer);
+    
+    Event._readyCallbacks.each(function(f) { f() });
+    Event._readyCallbacks = null;
+    
+  },
+  onReady : function(f) {
+    if (!this._readyCallbacks) {
+      var domReady = this._domReady;
+      
+      if (domReady.done) return f();
+      
+      if (document.addEventListener)
+        document.addEventListener("DOMContentLoaded", domReady, false);
+        
+        /*@cc_on @*/
+        /*@if (@_win32)
+            document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
+            document.getElementById("__ie_onload").onreadystatechange = function() {
+                if (this.readyState == "complete") { domReady(); }
+            };
+        /*@end @*/
+        
+        if (/WebKit/i.test(navigator.userAgent)) { 
+          this._timer = setInterval(function() {
+            if (/loaded|complete/.test(document.readyState)) domReady(); 
+          }, 10);
+        }
+        
+        Event.observe(window, 'load', domReady);
+        Event._readyCallbacks =  [];
+    }
+    Event._readyCallbacks.push(f);
+  }
+});
+
+Event.onReady(function() {
+
+  function selectPage(pageId) {
+    $$('#navigation li').each(function(menuItem) {
+      menuItem.removeClassName('selected');
+    });
+    
+    $('link-' + pageId).up('li').addClassName('selected');
+    
+    $$('.tab').each(function(tab) {
+      tab.addClassName('hidden');
+    });
+    
+    var tabId = 'tab-' + pageId;
+    $(tabId).removeClassName('hidden');
+    $(tabId).down('form').focusFirstElement();
+    
+    Cookies.save('selected_page', pageId);
+  }
+
+  $$('#header a').each(function(currentLink) {
+    $(currentLink).observe('click', function() {
+      var pageId = currentLink.id.replace('link-', '');
+      selectPage(pageId);
+    });
+  });
+  
+  var selectedPage = Cookies.read('selected_page')
+  selectPage(selectedPage ? selectedPage : 'php');
+  
+  $$('textarea').each(function(textarea) {
+    $(textarea).observe('keydown', function(event) {
+      if (event.ctrlKey && (13 == event.keyCode)) {
+        Tools.formHandler(textarea.up('form'));
+      }
+      
+      if (9 == event.keyCode) {
+        var tab = "    ";
+        var target = event.target;
+        var ss = target.selectionStart;
+        var se = target.selectionEnd;
+        
+        event.preventDefault();
+        
+        target.value = target.value.slice(0,ss).concat(tab).concat(target.value.slice(ss,target.value.length));
+        
+        if (ss == se) {
+          target.selectionStart = target.selectionEnd = ss + tab.length;
+        } else {
+          target.selectionStart = ss + tab.length;
+          target.selectionEnd = se + tab.length;
+        }
+      }
+    });
+  });
+  
+});
+
+
+Cookies = {
+  
+  save: function(name, value, days) {
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+  },
+  
+  read: function(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  },
+  
+  erase: function(name) {
+    createCookie(name,"",-1);
+  }
+  
+}
+
+Tools = {
+
+  formHandler: function(form) {
+    $(form).down('div.output-wrapper').update('');
+    
+    var contentElement = $(form).down('div.output-wrapper');
+    contentElement.update('');
+    var submitButton = $(form).down('input[type="submit"]');
+    submitButton.disable();
+    submitButton.addClassName("loading");
+    submitButton.value = 'Please, wait...';
+    
+    $(form).select('input', 'textarea').each(function(field) {
+      field.removeClassName('error');
+      field.title = '';
+    });
+    
+    new Ajax.Request(form.action, {
+      method: 'post',
+      parameters: Form.serialize(form),
+      onSuccess: function(transport) {
+        var response = transport.responseText.evalJSON();
+        
+        if ('undefined' != typeof response.errors) {
+          var errors = response.errors;
+          errors.each(function(error) {
+            var field = $(form).down('input[name="' + error.field + '"]');
+            
+            if (!field) {
+              field = $(form).down('textarea[name="' + error.field + '"]');
+            }
+            
+            field.addClassName('error');
+            field.title = error.text;
+          });
+        } else {
+          response.output = response.output.escapeHTML();
+          
+          if (!response.status) {
+            response.output = '<div class="error">Error</div>' + response.output;
+          }
+          
+          contentElement.update('<pre class="output" style="display: none">' + response.output + '</pre>');
+          new Effect.Appear(contentElement.down('pre'));
+        }
+        
+        submitButton.enable();
+        submitButton.removeClassName("loading");
+        submitButton.value = 'Execute';
+      }
+    });
+  }
+  
+}
